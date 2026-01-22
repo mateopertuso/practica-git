d3b62b11d99aa86ea24caf646c9ed44d4df5fd49
mateopertuso <mateopertuso.dev@gmail.com>
2026-01-22 12:19:04 -0300

    setup: initial tailwind setup and project cleanup

--- portafolio-teopertuso/src/app/app.component.ts
+++ portafolio-teopertuso/src/app/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.component.html',
+  styleUrl: '../styles/tailwind.css'
 })
 export class App {
   protected readonly title = signal('practicando-angular');
